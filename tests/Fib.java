c6617c05281181cff00beb55efc802755dc42f6e
Harald Steinlechner <haraldsteinlechner@gmail.com>
2012-03-27 22:13:42 +0200

    Utilities, BasicBlock into src folder, BasicBlock sketching

---
+++ tests/Fib.java
@@ -0,0 +1,13 @@
+public class Fib
+{
+	public static int fib(int n)
+	{
+		if(n<=1) return 1;
+		else return fib(n-1) + fib(n-2);
+	}
+
+	public static void main(String[] args)
+	{
+		fib(10);
+	}
+}
