95cc49e2567d0903d871a1672b059499f11e0291
Bernhard Urban <lewurm@gmail.com>
2012-04-26 21:04:14 +0200

    tools: script for installing the required haskell environment

---
+++ tools/installhaskellenv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+rm -rf ~/.ghc ~/.cabal
+cabal update
+cabal install cabal-install
+export PATH=~/.cabal/bin:$PATH
+cabal update
+cabal install hs-java --enable-shared
+cabal install harpy --enable-shared
+cabal install missingh --enable-shared
+cabal install heap --enable-shared
+cabal install plugins --enable-shared
+
+git clone git://wien.tomnetworks.com/harpy.git
+cd harpy
+cabal install --enable-shared
+cd ..
+rm -rf harpy
+
+git clone git://wien.tomnetworks.com/hs-java.git
+cd hs-java
+git checkout -t origin/v0.2
+cabal install --enable-shared
+cd ..
+rm -rf hs-java
+
+echo DONE
