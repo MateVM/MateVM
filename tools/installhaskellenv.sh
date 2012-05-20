933ec7b98b049bc7b5708a052d4224f07179bd93
Bernhard Urban <lewurm@gmail.com>
2012-05-20 22:56:24 +0200

    tools: install libs with profile information

--- tools/installhaskellenv.sh
+++ tools/installhaskellenv.sh
@@ -1,30 +1,31 @@
 #!/bin/bash -x
 
+CABAL_OPT="--enable-shared -p"
 rm -rf ~/.ghc ~/.cabal
 cabal update
-cabal install cabal-install --enable-shared
+cabal install cabal-install $CABAL_OPT
 export PATH=~/.cabal/bin:$PATH
 cabal update
-cabal install missingh --enable-shared
-cabal install heap --enable-shared
-cabal install plugins --enable-shared
-cabal install split --enable-shared
+cabal install missingh $CABAL_OPT
+cabal install heap $CABAL_OPT
+cabal install plugins $CABAL_OPT
+cabal install split $CABAL_OPT
 
 git clone git://wien.tomnetworks.com/disassembler.git
 cd disassembler
-cabal install --enable-shared
+cabal install $CABAL_OPT
 cd ..
 rm -rf disassembler
 
 git clone git://wien.tomnetworks.com/harpy.git
 cd harpy
-cabal install --enable-shared
+cabal install $CABAL_OPT
 cd ..
 rm -rf harpy
 
 git clone git://wien.tomnetworks.com/hs-java.git
 cd hs-java
-cabal install --enable-shared
+cabal install $CABAL_OPT
 cd ..
 rm -rf hs-java
 
