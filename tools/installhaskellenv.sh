84f98a331062d30a12e1e55460381a149e837d8f
Bernhard Urban <lewurm@gmail.com>
2012-08-02 21:37:24 +0200

    installenv: use custom harpy repo
    and provide a simple shell function for installing from a git
    repository

--- tools/installhaskellenv.sh
+++ tools/installhaskellenv.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -x
 
 CABAL_OPT="--enable-shared -p"
+
+function gitinstall {
+	url=$1
+	git clone $url tmprepo
+	cd tmprepo
+	cabal install $CABAL_OPT
+	cd ..
+	rm -rf tmprepo
+}
+
 rm -rf ~/.ghc ~/.cabal
 cabal update
 cabal install cabal-install $CABAL_OPT
@@ -11,13 +21,11 @@ cabal install heap $CABAL_OPT
 cabal install plugins $CABAL_OPT
 cabal install split $CABAL_OPT
 
-git clone git://wien.tomnetworks.com/hs-java.git
-cd hs-java
-cabal install $CABAL_OPT
-cd ..
-rm -rf hs-java
+# cabal install hs-java $CABAL_OPT
+gitinstall git://wien.tomnetworks.com/hs-java.git
 
 cabal install disassembler $CABAL_OPT
-cabal install harpy $CABAL_OPT
+# cabal install harpy $CABAL_OPT
+gitinstall git://wien.tomnetworks.com/harpy.git
 
 echo DONE
