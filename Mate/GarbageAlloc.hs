c74ea78bd7dc14b5c16d0733db02b47a91ff0d60
Bernhard Urban <lewurm@gmail.com>
2012-09-23 23:21:46 +0200

    alloc: clear allocated memory
    see java/lang/Runtime.java @ gnu classpath:
    > 94:   if (current != null)
    this is a line from the static initializer, and it was always true because the
    field member had some random value assigned. by clearing the allocated memory
    (as required by the jvm spec), the issue is fixed.

--- Mate/GarbageAlloc.hs
+++ Mate/GarbageAlloc.hs
@@ -12,6 +12,8 @@ module Mate.GarbageAlloc(
 import Foreign
 import Foreign.C
 
+import qualified Data.ByteString.Internal as BI
+
 import Mate.GC.Boehm
 
 import Mate.Debug
@@ -23,31 +25,38 @@ mallocClassData :: Int -> IO (Ptr a)
 mallocClassData size = do
   printfStr $ printf "mallocClassData: %d\n" size
   mem <- mallocBytes size
+  BI.memset (castPtr mem) 0 (fromIntegral size)
   addRootGC mem (plusPtr mem size)
   return mem
 
 mallocStringGC :: Int -> IO (Ptr a)
 mallocStringGC size = do
   printfStr $ printf "mallocString: %d\n" size
-  mallocBytesGC size
+  ptr <- mallocBytesGC size
+  BI.memset (castPtr ptr) 0 (fromIntegral size)
+  return ptr
 
 foreign export ccall mallocObjectGC :: Int -> IO CPtrdiff
 mallocObjectGC :: Int -> IO CPtrdiff
 mallocObjectGC size = do
   ptr <- mallocBytesGC size
+  BI.memset (castPtr ptr) 0 (fromIntegral size)
   printfStr $ printf "mallocObject: %d\n" size
   return $ fromIntegral $ ptrToIntPtr ptr
 
 mallocObjectUnmanaged :: Int -> IO CPtrdiff
 mallocObjectUnmanaged size = do
   ptr <- mallocBytes size
+  BI.memset (castPtr ptr) 0 (fromIntegral size)
   printfStr $ printf "mallocObjectUnmanged: %d\n" size
   return $ fromIntegral $ ptrToIntPtr ptr
 
 mallocStringUnmanaged :: Int -> IO (Ptr a)
 mallocStringUnmanaged size = do
   printfStr $ printf "mallocStringUnamaged: %d\n" size
-  mallocBytes size
+  ptr <- mallocBytes size
+  BI.memset (castPtr ptr) 0 (fromIntegral size)
+  return ptr
 
 
 getHeapMemory :: IO Int
