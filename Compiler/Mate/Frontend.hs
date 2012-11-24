30e4edc65bef3dbca2676413ca2ad0d8de39eca3
Bernhard Urban <lewurm@gmail.com>
2012-11-24 01:22:31 +0100

    frontend: use module shortcut

--- Compiler/Mate/Frontend.hs
+++ Compiler/Mate/Frontend.hs
@@ -1,15 +1,8 @@
-module Compiler.Mate.Frontend
-  ( module Compiler.Mate.Frontend.IR
-  , module Compiler.Mate.Frontend.Linear
-  , module Compiler.Mate.Frontend.MkGraph
---  , module Compiler.Mate.Frontend.PassOneUseDef
-  , module Compiler.Mate.Frontend.LivenessPass
-  , module Compiler.Mate.Frontend.RegisterAllocation
-  ) where
+module Compiler.Mate.Frontend (module X) where
 
-import Compiler.Mate.Frontend.IR
-import Compiler.Mate.Frontend.Linear
-import Compiler.Mate.Frontend.MkGraph
--- import Compiler.Mate.Frontend.PassOneUseDef
-import Compiler.Mate.Frontend.LivenessPass
-import Compiler.Mate.Frontend.RegisterAllocation
+import Compiler.Mate.Frontend.IR as X
+import Compiler.Mate.Frontend.Linear as X
+import Compiler.Mate.Frontend.MkGraph as X
+-- import Compiler.Mate.Frontend.PassOneUseDef as X
+import Compiler.Mate.Frontend.LivenessPass as X
+import Compiler.Mate.Frontend.RegisterAllocation as X
