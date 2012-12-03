c7d2a874b841f1f6aad8495d33602714f94d3864
Harald Steinlechner <haraldsteinlechner@gmail.com>
2012-12-03 21:36:16 +0100

    fixed broken build because Instance5 had a class Instance4 - sry

--- tests/Instance5.java
+++ tests/Instance5.java
@@ -1,9 +1,9 @@
 package tests;
 
-public class Instance4 {
+public class Instance5 {
 	public int a, b;
 
-	public Instance4() {
+	public Instance5() {
 
           Integer integer = new Integer(12);
 	//	System.out.printf("constr(default)\n");
@@ -11,12 +11,12 @@ public class Instance4 {
 		this.b = 0x1337;
 	}
 
-	public Instance4(int x) {
+	public Instance5(int x) {
 		this(x, 0x999);
 		System.out.printf("constr(1)\n");
 	}
 
-	public Instance4(int x, int y) {
+	public Instance5(int x, int y) {
 		System.out.printf("constr(2)\n");
 		this.a = x;
 		this.b = y;
@@ -24,13 +24,13 @@ public class Instance4 {
 
 	public static void main(String []args) {
 		System.out.printf("before calling\n");
-		Instance4 x = new Instance4();
+		Instance5 x = new Instance5();
 		System.out.printf("result: 0x%08x\n", x.a);
 		/*System.out.printf("result: 0x%08x\n", x.b);
-		x = new Instance4(0x333, 0x777);
+		x = new Instance5(0x333, 0x777);
 		System.out.printf("result: 0x%08x\n", x.a);
 		System.out.printf("result: 0x%08x\n", x.b);
-		x = new Instance4(0x444);
+		x = new Instance5(0x444);
 		System.out.printf("result: 0x%08x\n", x.a);
 		System.out.printf("result: 0x%08x\n", x.b); */
 	}
