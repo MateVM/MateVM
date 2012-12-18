eb16b87e03db9af2535787017a70b2de2972ad1a
Bernhard Urban <lewurm@gmail.com>
2012-12-18 15:00:09 +0100

    tests: force entry patching of interface method table

--- tests/Example.java
+++ tests/Example.java
@@ -6,6 +6,9 @@ public class Example extends A {
 		Example o = new Example();
 		o.method1();
 		o.method2();
+		((I1) o).method1();
+		((I2) o).method1();
+		((I2) o).method2();
 		System.out.printf("o.f1: %d\n", o.f1);
 	}
 }
@@ -14,7 +17,7 @@ class A implements I1, I2 {
 	int f1;
 	public void method1() { System.out.printf("method1\n"); }
 
-	public int method2() { f1 = 2; return f1; }
+	public int method2() { f1 += 2; return f1; }
 }
 
 interface I1 { void method1(); }
